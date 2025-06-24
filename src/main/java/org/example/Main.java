3202ca21bb6367b03c9ef118c5badd60af90c61c
Sivaraman M <msivaraman1998@gmail.com>
2025-06-24 10:51:46 +0530

    upload to github

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * Author: Sivaraman M
+ * User:Sivaraman M
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
